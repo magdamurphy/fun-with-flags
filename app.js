9ad3c01f5df642eaace6a87b5b0699b3b2cd5186
magdamurphy <magdalena2019@gmail.com>
2017-10-09 19:26:14 +0100

    modified

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+const first = document.getElementById('firstNumber').innerText
+const second = document.getElementById('secondNumber').innerText
+
+function addition(first, second) {
+  return parseInt(first) + parseInt(second)
+}
+
+function multiplication(first,socond) {
+
+    return parseInt(first) * parseInt(second)
+}
+
+const result = multiplication("20", "42")
+
+addition(firstN, secondN)
+
+document.getElementById("result").innerText = result
