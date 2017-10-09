f52e27cbb03eb29610cfdeb28ffede39bf7fd0e1
magdamurphy <magdalena2019@gmail.com>
2017-10-09 19:33:54 +0100

    countries api

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-
-const first = document.getElementById('firstNumber').innerText
-const second = document.getElementById('secondNumber').innerText
-
-function addition(first, second) {
-  return parseInt(first) + parseInt(second)
+//Create single new table cell with given text
+function createTableCell(text) {
+	const tableCell = document.createElement("td");
+	tableCell.innerHTML = text;
+	return tableCell;
 }
 
-function multiplication(first,socond) {
-
-    return parseInt(first) * parseInt(second)
+//Create single new table cell with image
+function createTableCellImg(img) {
+	const tableCell = document.createElement("td");
+	tableCell.appendChild(img);
+	return tableCell;
 }
 
-const result = multiplication("20", "42")
+//Create single new table row with text/img given as parameters
+function createTableRow(t1, t2, t3) {
+	row = document.createElement("tr");
+	const cellData = [t1, t2];
 
-addition(firstN, secondN)
+	cellData.forEach( function(element) {
+	  row.appendChild(createTableCell(element))});
 
-document.getElementById("result").innerText = result
+	row.appendChild(createTableCellImg(t3));
+
+	return row;
+}
+
+//Add created row to table
+function addRowToTable(t1, t2, t3) {
+  row = createTableRow(t1, t2, t3);
+  document.querySelector("tbody").appendChild(row);
+}
+
+//Create image element
+function createImg(data) {
+  const image = document.createElement("img");
+  const flag = data.flag;
+  image.src =  flag;
+  return image;
+
+}
+
+//import data from API
+const allCountries = fetch('https://restcountries.eu/rest/v2/all')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      json.map(function(data) {
+        addRowToTable(data.name, data.population.toLocaleString('en-UK'), createImg(data))});
+    });
+
